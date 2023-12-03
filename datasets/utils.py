8a6985effe2acd61b6cc5ade4921a2d22b2b1b84
Hamid <khalapoum71@gmail.com>
2023-12-04 00:34:19 +0100

    function deal_with_NA_values

--- datasets/utils.py
+++ datasets/utils.py
@@ -9,7 +9,7 @@ import matplotlib.pyplot as plt
 -> sinon mettre une nouvelle catégorie 
 """
 
-def deal_with_NA_values(df, column, r):
+def deal_with_NA_values(df, column, r, r_float):
     """This function deals with NA values:
     - if the number of NA values is high (according to ratio r) create a new category "unknown"
     - else: replace by the majoritary category
@@ -18,11 +18,35 @@ def deal_with_NA_values(df, column, r):
         df (_DataFrame_): data
         column (_string_): name of the column
         r (_float_): ratio between the number of NA values and the number of values in minority category
-
+        r_float (_float_) : ratio between number of categories and length of the column. In order to consider if the column is categorical or not
     Returns:
-        _type_: _description_
+        df (_pd_dataframe_): dataframe containing the column with dealed Nan problem
     """
 
     categories_counts = df[column].value_counts(dropna=True)
+    Nan_counts = df[column].size - sum(categories_counts)
     
-    return categories_counts
+    if df[column].dtype == 'float' : 
+        # Verify if its categorical or take any float values
+        
+        # Categorical float value
+        if categories_counts.size/df[column].size <= r_float : 
+            if Nan_counts/df[column].size <= r : # Don't create a new class
+                return df[column].fillna(categories_counts.idxmax())
+            else :
+                return df[column].fillna(np.mean(df[column]))
+            
+        else :
+            # When it's not categorical compute the mean 
+            return df[column].fillna(np.mean(df[column]))
+
+    else :
+        # We consider as categorical all column with other non float type
+        if Nan_counts/df[column].size <= r : # Don't create a new class
+            return df[column].fillna(categories_counts.idxmax())
+        else :
+            return df[column].fillna("unknown")
+            
+        
+        
+    
