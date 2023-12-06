a99cd09d8ce29f0f0297e3d83648e41fea4670d1
YassKa71 <kaddyassine22@gmail.com>
2023-12-06 17:42:27 +0100

    Na values for all dataframe

--- utils.py
+++ utils.py
@@ -21,7 +21,7 @@ import matplotlib.pyplot as plt
 9- testing (score?)
 """
 
-def deal_with_NA_values(df, column, r, r_float):
+def deal_with_NA_values(df, r, r_float):
     """This function deals with NA values:
     - if the number of NA values is high (according to ratio r) create a new category "unknown"
     - else: replace by the majoritary category
@@ -34,46 +34,51 @@ def deal_with_NA_values(df, column, r, r_float):
     Returns:
         df (_pd_dataframe_): dataframe containing the column with dealed Nan problem
     """
-
-    categories_counts = df[column].value_counts(dropna=True)
-    Nan_counts = df[column].size - sum(categories_counts)
-    
-    if df[column].dtype == 'float' : 
-        # Verify if its categorical or take any float values
+    for column in df.columns:
+        categories_counts = df[column].value_counts(dropna=True)
+        Nan_counts = df[column].size - sum(categories_counts)
         
-        # Categorical float value
-        if categories_counts.size/df[column].size <= r_float : 
-            if Nan_counts/df[column].size <= r : # Don't create a new class
-                return df[column].fillna(categories_counts.idxmax())
+        if df[column].dtype == 'float' : 
+            # Verify if its categorical or take any float values
+            
+            # Categorical float value
+            if categories_counts.size/df[column].size <= r_float : 
+                if Nan_counts/df[column].size <= r : # Don't create a new class
+                    df[column]= df[column].fillna(categories_counts.idxmax())
+                else :
+                    df[column] = df[column].fillna(np.mean(df[column]))
+                
             else :
-                return df[column].fillna(np.mean(df[column]))
-            
-        else :
-            # When it's not categorical compute the mean 
-            return df[column].fillna(np.mean(df[column]))
+                # When it's not categorical compute the mean 
+                df[column] = df[column].fillna(np.mean(df[column]))
 
-    else :
-        # We consider as categorical all column with other non float type
-        if Nan_counts/df[column].size <= r : # Don't create a new class
-            return df[column].fillna(categories_counts.idxmax())
         else :
-            return df[column].fillna("unknown")
+            # We consider as categorical all column with other non float type
+            if Nan_counts/df[column].size <= r : # Don't create a new class
+                df[column] = df[column].fillna(categories_counts.idxmax())
+            else :
+                df[column] = df[column].fillna("unknown")
+    return df
             
 
-def one_hot_encoding(df):
+def one_hot_encoding(df, target, columns=[]):
     """Apply one hot encoding on categorical columns of df.
 
     Args:
         df (DataFrame): data
+        columns (list): name of columns to be one hot encoded
+        target (string): name of target column. If target is not present in the df, specify None.
 
     Returns:
         DataFrame: new one hot encoded dataframe
     """
-    categorical_columns = df.select_dtypes(include=['object']).columns.tolist()
-    """for col in categorical_columns:
-        df[col] = df[col].astype('category')"""
-    encoded_columns = pd.get_dummies(df, columns=categorical_columns)
-    df = df.drop(categorical_columns, axis=1)
+    if target is not None:
+        df = df.drop(target, axis=1)
+    if columns == []:
+        encoded_columns = pd.get_dummies(df)
+    else:
+        encoded_columns = pd.get_dummies(df, columns=columns)
+    df = df.drop(encoded_columns.columns.tolist(), axis=1)
     df_encoded = pd.concat([df, encoded_columns], axis=1)
     return df_encoded
     
